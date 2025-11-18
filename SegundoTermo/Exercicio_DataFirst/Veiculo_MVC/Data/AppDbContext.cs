7ff39cc6a7f087c2be91e46c251884c30614c803
DiegoCastro2610 <diego.c.rocha6@aluno.senai.br>
2025-11-18 10:46:07 -0300

    18_11_25

---
+++ SegundoTermo/Exercicio_DataFirst/Veiculo_MVC/Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Veiculo_MVC.Models;
+
+namespace Veiculo_MVC.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<TabelaVeiculo> TabelaVeiculo { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SegundoTermo/Exercicio_DataFirst/Veiculo_MVC/Models/TabelaVeiculo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Veiculo_MVC.Models;
+
+[Table("TabelaVeiculo")]
+public partial class TabelaVeiculo
+{
+    [Key]
+    public int Id { get; set; }
+
+    [StringLength(25)]
+    [Unicode(false)]
+    public string Modelo { get; set; } = null!;
+
+    public int Ano { get; set; }
+
+    [StringLength(25)]
+    [Unicode(false)]
+    public string Classe { get; set; } = null!;
+}
