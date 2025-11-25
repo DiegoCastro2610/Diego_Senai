35c3ad9de6f7b7a8669ad73acc6cde5767913581
DiegoCastro2610 <diego.c.rocha6@aluno.senai.br>
2025-11-25 10:47:30 -0300

    25_11_25

---
+++ SegundoTermo/Projeto Aurun MVC/AurumLab/Controllers/LoginController.cs
@@ -0,0 +1,60 @@
+using Microsoft.AspNetCore.Mvc;
+using AurumLab.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using AurumLab.Service;
+
+
+namespace AurumLab.Controllers
+{
+    public class LoginController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public LoginController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Entrar(string email, string senha)
+        {
+            if(string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(senha))
+            {
+                ViewBag.Error = "Preencha todos os acampos";
+                return View("Index");
+            }
+
+            byte[] senhaDigitadaHash = HashService.GerarHashBytes(senha);
+
+            var usuario = _context.Usuarios.FirstOrDefault(usuario => usuario.Email == email);
+            if(usuario == null)
+            {
+                ViewBag.Error = "E-mail ou senha incorreta";
+                return View("Index");
+            }
+
+            if(!usuario.Senha.SequenceEqual(senhaDigitadaHash))
+            {
+                ViewBag.Error = "E-mail ou senha incorreta";
+                return View("Index");
+            }
+
+            HttpContext.Session.SetString("UsuarioNome", usuario.NomeCompleto);
+            HttpContext.Session.SetInt32("usuarioId", usuario.IdUsuario);
+
+            return RedirectToAction("Dashboard", "Dashboard");
+        }
+
+        public IActionResult Sair()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+    }
+}
